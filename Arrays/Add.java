ef98a7c46936650ed8220523b7b7a15e603b5171
Ankit-0909 <ankitblade646@gmail.com>
2025-07-12 21:42:05 +0530

    Update Add.java
    Cleaning Code

--- Arrays/Add.java
+++ Arrays/Add.java
@@ -3,9 +3,7 @@ public class Addition{
         //  ----> sum of all element of an array
         int [] marks = {2,4,3,6,8 };
         int sum = 0;
-        // for(int element:marks){
-        //  sum = sum + element;
-        //}
+    
         //System.out.println(sum);
         for(int i = 0;i<marks.length;i++){
             sum = sum +marks[i];
